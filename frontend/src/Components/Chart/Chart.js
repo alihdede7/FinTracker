d469b0854bc2ab5e4972198511c7ca5b0f2b8ceb
alihdede7 <alihdede7@gmail.com>
2024-08-13 00:26:57 +0300

    updated frontend

---
+++ frontend/src/Components/Chart/Chart.js
@@ -0,0 +1,90 @@
+import React from 'react'
+import {Chart as ChartJs, 
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend,
+    ArcElement,
+} from 'chart.js'
+
+import {Line} from 'react-chartjs-2'
+import styled from 'styled-components'
+import { useGlobalContext } from '../../context/globalContext'
+import { dateFormat } from '../../utils/dateFormat'
+
+ChartJs.register(
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend,
+    ArcElement,
+)
+
+function Chart() {
+    const {incomes, expenses} = useGlobalContext()
+
+    const data = {
+        labels: incomes.map((inc) =>{
+            const {date} = inc
+            return dateFormat(date)
+        }).reverse(),
+        datasets: [
+            {
+                label: 'Income',
+                data: [
+                    ...incomes.map((income) => {
+                        const {amount} = income
+                        return amount
+                    }).reverse()
+                ],
+                backgroundColor: 'green', // light green background for the line area
+                borderColor: 'green', // green line color
+                tension: .2
+            },
+            {
+                label: 'Expenses',
+                data: [
+                    ...expenses.map((expense) => {
+                        const {amount} = expense
+                        return amount
+                    })
+                ],
+                backgroundColor: 'red', // light red background for the line area
+                borderColor: 'red', // red line color
+                tension: .2
+            }
+        ]
+    }
+
+    const options = {
+        scales: {
+            y: {
+                beginAtZero: true // This ensures the y-axis starts at 0
+            }
+        }
+    };
+
+
+    return (
+        <ChartStyled >
+            <Line data={data} options={options}/>
+        </ChartStyled>
+    )
+}
+
+const ChartStyled = styled.div`
+    background: #FCF6F9;
+    border: 2px solid #FFFFFF;
+    box-shadow: 0px 1px 15px rgba(0, 0, 0, 0.06);
+    padding: 1rem;
+    border-radius: 20px;
+    height: 100%;
+`;
+
+export default Chart
